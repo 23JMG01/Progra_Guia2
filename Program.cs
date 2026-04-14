05df6847f71a9bae7011889de7a03a47cdf84393
23JMG01 <javim2801@gmail.com>
2026-04-13 18:56:03 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,52 +4,20 @@ class Program
 {
     static void Main()
     {
-        int num;
+        Console.Write("Ingrese un carácter: ");
+        char c = Convert.ToChar(Console.ReadLine());
 
-        Console.Write("Ingrese un número del 1 al 12: ");
-        num = int.Parse(Console.ReadLine());
-
-        switch (num)
+        if (char.IsDigit(c))
         {
-            case 1:
-                Console.WriteLine("Enero - 31 días");
-                break;
-            case 2:
-                Console.WriteLine("Febrero - 28 días");
-                break;
-            case 3:
-                Console.WriteLine("Marzo - 31 días");
-                break;
-            case 4:
-                Console.WriteLine("Abril - 30 días");
-                break;
-            case 5:
-                Console.WriteLine("Mayo - 31 días");
-                break;
-            case 6:
-                Console.WriteLine("Junio - 30 días");
-                break;
-            case 7:
-                Console.WriteLine("Julio - 31 días");
-                break;
-            case 8:
-                Console.WriteLine("Agosto - 31 días");
-                break;
-            case 9:
-                Console.WriteLine("Septiembre - 30 días");
-                break;
-            case 10:
-                Console.WriteLine("Octubre - 31 días");
-                break;
-            case 11:
-                Console.WriteLine("Noviembre - 30 días");
-                break;
-            case 12:
-                Console.WriteLine("Diciembre - 31 días");
-                break;
-            default:
-                Console.WriteLine("Número inválido");
-                break;
+            Console.WriteLine("Es un dígito");
+        }
+        else
+        {
+            char letra = char.ToUpper(c);
+            if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
+                Console.WriteLine("Es una vocal");
+            else
+                Console.WriteLine("No es vocal ni dígito");
         }
     }
 }
